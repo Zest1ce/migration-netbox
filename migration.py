e397fbec6ea78a37db921f3bfb2d3fde44ef5d51
Anton Zest1ce <anton1kar@yandex.ru>
2025-07-04 12:44:38 +0300

    update

--- migration.py
+++ migration.py
@@ -170,6 +170,29 @@ def get_phpipam_devices(phpipam_api_endpoint):
             return None
     except requests.exceptions.RequestException as error_api:
         print(f"Произошла ошибка при подключении к эндпоинту phpIPAM: {error_api}")
+
+# Функция для получения типа Devices
+@backoff.on_exception(backoff.expo,
+                      (requests.exceptions.RequestException, 
+                       JSONDecodeError), 
+                       max_tries=5)
+def get_phpipam_devices_type(type_id):
+    try:
+        url = f"{PHPIPAM_URL}/{PHPIPAM_APP_ID}/tools/device_types/{type_id}/" # Формируем URL для запроса данных о типе девайсе
+        print(f"Обработка запроса по эндпоинту: {url}")
+        response = requests.get(url, headers=HEADERS_PHPIPAM, verify=False)
+        if response.status_code == 200:
+            print("Успешный запрос данных из phpIPAM.")
+            tname = response.json().get("data", []).get("tname")
+            tdescription = response.json().get("data", []).get("tdescription")
+            return tname, tdescription;
+        else:
+            print(f"Ошибка при запросе данных: {response.status_code}")
+            print(response.text)
+            return None
+    except requests.exceptions.RequestException as error_api:
+        print(f"Произошла ошибка при подключении к эндпоинту phpIPAM: {error_api}")
+
 # Функция для открытия разных json файлов
 def load_data_from_json(file_path):
     """Загрузка данных из JSON-файла."""
@@ -368,6 +391,112 @@ def create_addresses_in_netbox():
     bar.finish()
     print(f"\nВсего обработано IP адресов: {processed_count} из {total_ip}")
 
+# Перенос девайсов(Обработчик для запросов из phpIPAM в NetBOX) 
+@backoff.on_exception(backoff.expo,
+                      (requests.exceptions.RequestException, 
+                       JSONDecodeError), 
+                       max_tries=5)
+def create_devices_in_netbox():
+    print('Запуск обработчика json - добавление данных: devices')
+    locations_data = load_data_from_json('./data/phpipam_data_location.json').get("data", [])
+    devices_data = load_data_from_json('./data/phpipam_data_devices.json').get("data", [])
+
+    # Создание словаря для быстрого доступа к локации по id
+    location_id_to_address = {
+        location["id"]: {
+            "name": location.get("name"),
+            "description": location.get("description"),
+            "address": location.get("address"),
+            "lat": location.get("lat"),
+            "long": location.get("long"),
+        }
+        for location in locations_data
+    }
+    print(location_id_to_address)
+    # Определяем диапазон ID
+    id_range = (2, 20000)
+    total_devices = len([s for s in devices_data if id_range[0] <= int(s["id"]) <= id_range[1]])
+    bar = IncrementalBar("Обработка девайсов", max=total_devices)
+    processed_count = 0
+    for device in devices_data:
+        device_id = int(device["id"])
+        if not (id_range[0] <= device_id <= id_range[1]):
+            continue
+        
+        id_type = device.get("type")
+        device_type = get_phpipam_devices_type(id_type)
+
+        # Очищаем значения hostname от символом которые не принимает Netbox, в netbox нет hostname, вместо него dns name
+        dns_name = device.get("hostname", "") or ""
+        dns_name = translit(dns_name, 'ru', reversed=True)
+        dns_name = re.sub(r'[^a-zA-Z0-9._*-]', '', dns_name)
+        # Формируем slug для location
+        location_data = location_id_to_address.get(device.get("location"), {})
+        
+        location_slug = location_data.get("name") or ""
+        location_slug = clean_slug(location_slug)
+        site_data = {
+            "name": location_data.get("name", ""),
+            "slug": location_slug,
+            "description": location_data.get("description", ""),
+            "physical_address": location_data.get("address", ""),
+            "latitude": location_data.get("lat", None),
+            "longitude": location_data.get("long", None),
+        }
+        print(site_data)
+        
+        # Формируем данные для отправки в NetBox
+        device_data = {
+            "name": dns_name,
+            "device_type": {
+                "manufacturer": {
+                "name": device.get("custom_Vendor", "") or "",
+                "slug": clean_slug(device.get("custom_Vendor", "")) or "",
+                "description": "",
+                },
+                "model": device.get("custom_Model", "") or "",
+                "slug": clean_slug(device.get("custom_Model")) or "",
+                "description": "",
+            },
+            "role": {
+                "name": f"{device_type[0]}",
+                "slug": clean_slug(device_type[0]),
+                "description": f"{device_type[1]}",
+            },
+            "platform": {
+                "name": device.get("custom_Firmware", "") or "",
+                "slug": clean_slug(device.get("custom_Firmware")) or "",
+                "description": ""
+            },
+            "serial": device.get("custom_Serial", "") or "",
+            "site": {
+                "name": site_data.get("name"),
+                "slug": site_data.get("slug"),
+                "description": site_data.get("description"),
+            },
+            "latitude": site_data.get("latitude"),
+            "longitude": site_data.get("longitude"),
+            "status": "active",
+            "primary_ip4": {
+                "address": device.get("ip", "") or "",
+                "description": ""
+            },
+            "description": device.get("description", "") or "",
+            "comments": device.get("description", "") or "",
+        }
+        print(device_data)
+        # Отправляем данные в NetBox
+        response = requests.post(f"{NETBOX_URL}/dcim/devices/", headers=HEADERS_NETBOX, json=device_data, verify=False)
+        if response.status_code == 201:
+            print(f"Устройство {device_data['name']} успешно создано в NetBox.")
+            processed_count += 1
+        else:
+            print(f"Ошибка при создании устройства {device_data['name']}: {response.status_code}")
+            print(response.text)
+        bar.next()
+    bar.finish()
+    print(f"\nВсего обработано устройств: {processed_count} из {total_devices}")
+
 def generate_location_json():
     """
     Генерирует JSON-файл с данными о локациях из JSON-файла с подсетями.
@@ -438,8 +567,9 @@ def main_function():
     else:
         print("Не удалось получить данные из phpIPAM.")
     # Запуск обработчика json
-    create_subnets_in_netbox()
-    create_addresses_in_netbox()
+    #create_subnets_in_netbox()
+    #create_addresses_in_netbox()
+    create_devices_in_netbox()
 
 main_function()
 
